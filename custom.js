7be2d5bdf1ad90300d7f492a5ea869b123c5e799
enocklan <enockkipkirui003@gmail.com>
2026-09-23 12:19:46 +0300

    intial commmit

---
+++ custom.js
@@ -0,0 +1,17 @@
+
+// = equal operator(loose equal operator)
+// == equal operator(strict equal operator)
+// != not equal operator(loose not equal operator)
+//< less than operator
+//> greater than operator
+//<= less than or equal to operator
+//>= greater than or equal to operator
+
+let age=11
+let height=6
+if (age>=18 || height>=6){
+    console.log(" True  ");
+}
+else{
+    console.log(" False ");
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+// variable declaration using const
+const studentName = "John Doe";
+
+console.log(studentName) 
+// variabble declaration  using var
+
+var x=5
+x=10
+console.log(x)
+// variable declaration using let
+let y = x+15
+ y=20
+console.log(y)
