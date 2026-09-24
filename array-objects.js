a550e13a77f288de013a6f0982717fcc16dcf13c
enocklan <enockkipkirui003@gmail.com>
2026-09-24 10:41:46 +0300

    arrays and objects

---
+++ array-objects.js
@@ -0,0 +1,27 @@
+let studentNames = ["John", "Jane", "Mark"]
+let indexZero = studentNames[0]
+studentNames.push("Waleed")
+studentNames.push("Duncan")
+let studentNamesLength = studentNames.length
+let lastItem= studentNames[studentNames.lenght-1]
+
+// console.log(lastItem)
+
+// let arrayItems =["john", 1 ,]
+
+// console.log(arrayItems[arrayItems.length-1])
+// objects
+// objects
+let car = {
+  model: "Mercedes",
+  yom: 2024,
+  color: "Black",
+  millage:null
+}
+carModel = car["model"]
+carColor = car.color
+carMillage=car.millage
+// console.log([carModel, carColor, carMillage])
+// check the datatype using typeof keyword
+
+console.log(typeof car.millage)
