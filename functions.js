0893dd6b667cdf90aa099f9e096659d8d5412b96
enocklan <enockkipkirui003@gmail.com>
2026-09-25 11:10:00 +0300

    js fuctions

---
+++ functions.js
@@ -0,0 +1,12 @@
+function addNumbers(num1, num2) { // Function definition with parameters
+
+  let sum = num1 + num2; // Local variable 'sum' stores the result
+
+  return sum; // Return the calculated sum
+
+}
+
+let result = addNumbers(5, 3); // Function call with arguments (5, 3)
+
+
+console.log("The sum is:", result); // Use the returned value (8)
