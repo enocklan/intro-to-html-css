55b892b63cabec35538577e10aaba27e97da2581
enocklan <enockkipkirui003@gmail.com>
2026-09-25 09:06:51 +0300

    update on variables

--- dsj.js
+++ dsj.js
@@ -1,1 +1,30 @@
-let cars = ['Mercedes','bmw', 'Audi']
+// String
+const name = "Jack"; // <= quotes tell us this is a string
+
+// Number
+const favNum = 29;
+
+// Boolean
+const iLoveCoding = true; // <= no quotes
+const iLoveGrapes = false;
+
+// Null
+const blackHole = null;
+
+// Undefined
+let myName; // here we declared the variable, 
+             // but never gave it a value, so 
+             // it is Undefined
+
+// Object
+const person = {
+  name: "John Doe",
+  dob: "02/23/99", 
+  weight: 70,
+  hairColor: "brown" 
+}
+
+// Array
+const fruits = ["apples", "oranges", "pears", "bananas"];
+const numArray = [1, 2, 3, 4, 5];
+const myArray = [21, "pear", [3, 7, 22], { objectsToo: true }];
